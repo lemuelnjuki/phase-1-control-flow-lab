90bbe85996a561abe9f7f5d5547559118eec59bd
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-16 13:51:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let answer;
+  if (feet <= 400){
+    answer = "This one is on me!";
+  }
+  else if(feet>2500){
+    answer = "No can do.";
+  }
+  else if(feet>2000){
+    answer = "I will gladly take your thirty bucks.";
+  }
+  return answer;
 }
+ 
 
-function ternaryCheckCity(){
+let city ='';
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if(city === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if(city !== 'NYC'){
+    return 'No go.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+let tip ='';
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if(tip === 'generous'){
+    return 'Thank you so much.';
+  } 
+  else if (tip === 'not as generous'){
+    return 'Thank you.';
+  }
+  else{return 'Bye.';
+  }
+} 
